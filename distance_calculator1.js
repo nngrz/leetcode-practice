7d096d4c1fa86089da5646b0e21635ef37ea23c4
Ningrui Zhang <ningrui1227@gmail.com>
2025-08-15 13:03:32 +0100

    add distance_calculator1.js

---
+++ distance_calculator1.js
@@ -0,0 +1,29 @@
+var distance_calculator = function(distance) {
+    const steps = [1, 2, 3];
+    const map = new Map();
+    for (let i = 0; i < steps.length; i++) {
+        map.set(steps[i], i);
+    }
+
+    let result = 0;
+    for (let i = 0; i < steps.length; i++) {
+        const rest_distance = distance - steps[i];
+        if (distance == 1) {
+            return result = 1;
+        } else {
+            if (map.has(rest_distance)) {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+
+const distance1 = 1;
+console.log(distance_calculator(1));
+
+const distance2 = 2;
+console.log(distance_calculator(2));
+
+const distance3 = 3;
+console.log(distance_calculator(3));
