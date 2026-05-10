34f6ddbf9d0f988ae75e1a5a826bead63e389fa9
Ningrui Zhang <ningrui1227@gmail.com>
2026-05-10 21:45:42 +0100

    improve the time complexity

--- AssignCookies.java
+++ AssignCookies.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        /*
         Arrays.sort(g);
         Arrays.sort(s);
 
@@ -20,6 +21,21 @@ class Solution {
         }
 
         return count;
+        */
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
     }
 }
 
