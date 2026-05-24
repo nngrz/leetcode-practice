99c9e3a2f251708e6302db6ef0030476dcc9c323
Ningrui Zhang <ningrui1227@gmail.com>
2026-05-24 23:08:28 +0100

    add file FindTheDuplicateNumber.java

---
+++ FindTheDuplicateNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+
+    }
+}
+
+public class FindTheDuplicateNumber {
+    public static void Main(String[] args) {
+        
+    }
+}
